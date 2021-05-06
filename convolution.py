15ab602eaa84fce282f9b6c8cbb10d79974f9cb1
Arisbeth Aguirre <a01274803@itesm.mx>
2021-05-06 15:40:24 -0500

    Modificación de función addpadding

--- convolution.py
+++ convolution.py
@@ -5,8 +5,8 @@ import random
 import cv2
 import argparse
 
-def total_conv(padding,imagen,filtro):
-    imagen = addpadding(padding,imagen)
+def total_conv(imagen,filtro,verbose=False):
+    imagen = addpadding(imagen,filtro,verbose)
     print(imagen)
     x=len(imagen)- len(filtro)+1
     y=len(imagen[0])- len(filtro)+1
@@ -20,8 +20,7 @@ def total_conv(padding,imagen,filtro):
 
     return res
 
-def addpadding(padding,imagen):
-
+def addpadding(imagen, kernel, verbose=False):
     if len(imagen.shape) == 3:
         print("Found 3 Channels : {}".format(imagen.shape))
         imagen = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -29,13 +28,23 @@ def addpadding(padding,imagen):
     else:
         print("Image Shape : {}".format(imagen.shape))
 
-    x=len(imagen) + padding*2
-    y=len(imagen[0]) + padding*2
+
+    x_kernel, y_kernel = kernel.shape
+
+    pad_x = int((x_kernel-1)/2)
+    pad_y = int((y_kernel-1)/2)
+    
+    x = len(imagen) + pad_x*2
+    y = len(imagen[0]) + pad_y*2
+
+
     imagenPadding = np.zeros((x,y))
-    for ren in range(padding, len(imagenPadding)-padding):
-        for col in range(padding, len(imagenPadding[0])-padding):
-            imagenPadding[ren,col] = imagen[ren - padding,col - padding]
+    for ren in range(pad_x, len(imagenPadding)-pad_x):
+        for col in range(pad_y, len(imagenPadding[0])-pad_y):
+            imagenPadding[ren,col] = imagen[ren - pad_x,col - pad_y]
+            
     return imagenPadding
+
 
 
 filtro = np.random.randint(10,size =(3,3))
@@ -46,4 +55,4 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
     image = cv2.imread(args["image"])
 
-print(total_conv(2,image,filtro))
+print(total_conv(image,filtro,verbose=True))
